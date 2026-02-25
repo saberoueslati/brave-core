951ffdc9ce19cbf6834a522db18f9a6ead159b89
Darnell Andries <dandries@brave.com>
2026-02-25 15:29:39 -0800

    Add credential manager to `AIChatMetrics`

--- components/ai_chat/core/browser/ai_chat_service.cc
+++ components/ai_chat/core/browser/ai_chat_service.cc
@@ -160,9 +160,6 @@ AIChatService::AIChatService(
                           weak_ptr_factory_.GetWeakPtr()));
 
   MaybeInitStorage();
-
-  // Get current premium status to report metrics
-  GetPremiumStatus(base::DoNothing());
 }
 
 AIChatService::~AIChatService() = default;
@@ -728,10 +725,6 @@ void AIChatService::OnPremiumStatusReceived(GetPremiumStatusCallback callback,
 #endif
 
   last_premium_status_ = status;
-  if (ai_chat_metrics_ != nullptr) {
-    ai_chat_metrics_->OnPremiumStatusUpdated(
-        ai_chat::HasUserOptedIn(profile_prefs_), false, status, info.Clone());
-  }
   model_service_->OnPremiumStatus(status);
   std::move(callback).Run(status, std::move(info));
 }
@@ -1162,7 +1155,7 @@ void AIChatService::OnUserOptedIn() {
     kv.second->OnUserOptedIn();
   }
   if (ai_chat_metrics_ != nullptr) {
-    ai_chat_metrics_->RecordEnabled(true, true, {});
+    ai_chat_metrics_->RecordEnabled(/*is_new_user=*/true);
   }
 }
 
